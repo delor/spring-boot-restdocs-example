7429eb3741cf717e0c3e5948d500a3c24054c67b
Bartłomiej Piech <bartekpiech@gmail.com>
2018-09-21 10:46:17 +0200

    init spring

---
+++ src/main/java/com/example/restdoc/RestdocApplication.java
@@ -0,0 +1,12 @@
+package com.example.restdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestdocApplication.class, args);
+    }
+}
